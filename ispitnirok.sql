9c36907d35fb272f5f0402a41b5e4a7ae0a8b449
LeaBartos <lotalulea@gmail.com>
2024-11-27 21:00:52 +0100

    Create ispitnirok.sql

---
+++ ispitnirok.sql
@@ -0,0 +1,25 @@
+﻿use master;
+go
+drop database if exist ispitnirok;
+go
+create database ispitnirok;
+go
+
+use ispitnirok;
+
+create table ispitnirok(
+sifra int,
+predmet varchar (50),
+vrsta ispita varchar (10),
+datum datetime,
+pristupio bit
+);
+
+create table pristupnici (
+ispitnirok varchar,
+student varchar,
+brojbodova char,
+ocjena varchar
+);
+
+
