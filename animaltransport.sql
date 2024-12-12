2840ab794c0bb82df91c04cf7d7e4ef489230172
LeaBartos <lotalulea@gmail.com>
2024-12-12 14:46:59 +0100

    Update animaltransport.sql

--- animaltransport.sql
+++ animaltransport.sql
@@ -48,8 +48,8 @@ vrsta_zivotinje, kolicina, nacin_placanja,
 narucitelj, prijevoznik ) values
 
 ( 'Automobil', 'Osijek', 'Zagreb', '2024-11-12', 
-'Pas', '2', 'Gotovina', 'Lea Bartos', 
-'Monaline Professional');
+'Pas', '2', 'Gotovina', 'Lea_Bartos', 
+'Monaline_Professional_Transport');
 
 --2
 insert into prijevozi ( vrsta, polaziste, odrediste, datum, 
@@ -57,19 +57,48 @@ vrsta_zivotinje, kolicina, nacin_placanja,
 narucitelj, prijevoznik ) values
 
 ( 'Kombi', 'Osijek', 'Rijeka', '2024-12-12', 
-'Macka', '3', 'Online placanje karticom', 'Gordana Grnja', 
-'Demian Transport');
-
+'Macka', '3', 'Karticno_placanje', 'Gordana_Grnja', 
+'Demian_Transport');
 
 --3
+insert into prijevozi ( vrsta, polaziste, odrediste, datum, 
+vrsta_zivotinje, kolicina, nacin_placanja, 
+narucitelj, prijevoznik ) values
+
+( 'Kamion', 'Osijek', 'Split', '2024-13-12', 
+'Konj', '5', 'Placanje_putem_e-racuna', 'Pijunovic Company', 
+'Horse_Transport');
+
+--4
 insert into narucitelji ( vrsta, ime, prezime, adresa, 
 telefon, e-mail ) values
 
-( 'Fizicka_osoba', 'Lea', 'Bartos', 'Osijek', '098765432', 'leabartos@gmail.com');
+( 'Fizicka_osoba', '1', '1', 'Osijek', '098765432', 'leabartos@gmail.com');
 
---3
+--5
+insert into narucitelji ( vrsta, ime, prezime, adresa, 
+telefon, e-mail ) values
+( 'Fizicka_osoba', '2', '2', 'Osijek', '0915664432', 'gordanagrnja@gmail.com');
+
+--6
+insert into narucitelji ( vrsta, ime, prezime, adresa, 
+telefon, e-mail ) values
+( 'Pravna osoba', '3', '3', 'Osijek', '0989701878', 'pijunovic@gmail.com');
+
+--7
 insert into prijevoznici ( naziv, oib, broj_licence, adresa, 
 telefon, e-mail ) values
 
-('Monaline_Professional', '123456789123', 'HR12345678', 'Kralja_Tomislava_7',
-'monaline@gmail.com');
+( '1', '16092240758', 'HR10032100', 'Sjenjak_28', '031573482', 'mpro@gmail.com');
+
+--8
+insert into prijevoznici ( naziv, oib, broj_licence, adresa, 
+telefon, e-mail ) values
+
+( '2', '76545578987', 'HR10032110', 'Ruzina_32', '031456654', 'demian@gmail.com');
+
+--9
+insert into prijevoznici ( naziv, oib, broj_licence, adresa, 
+telefon, e-mail ) values
+
+( '3', '23445543123', 'HR10032132', 'Cvjetkova_110', '031100987', 'horse@gmail.com');
