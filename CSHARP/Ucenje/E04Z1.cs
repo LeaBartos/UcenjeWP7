f8a6ffc11c4893b40a6aa583a6450b958aeda92a
LeaBartos <lotalulea@gmail.com>
2025-01-15 14:47:33 +0100

    E05UvjetnoGrananjeSwitch

--- CSHARP/Ucenje/E04Z1.cs
+++ CSHARP/Ucenje/E04Z1.cs
@@ -8,5 +8,30 @@ namespace Ucenje
 {
     internal class E04Z1
     {
+
+        // Program unosi od korisnika cijeli broj.
+        // Program ispuje da li je broj paran ili neparan
+
+        public static void Izvedi()
+        {
+            Console.Write("Unesi cijeli broj: ");
+            int broj = int.Parse(Console.ReadLine());
+
+            if (broj % 2 == 0)
+            {
+                Console.WriteLine("PARAN");
+            }
+            else
+            {
+                Console.WriteLine("NEPARAN");
+            }
+
+            Console.WriteLine("{0}PARAN", broj % 2 == 0 ? "" : "NE");
+
+        }
+
     }
 }
+
+
+
