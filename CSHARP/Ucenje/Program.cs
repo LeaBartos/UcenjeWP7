f3255f8174c74feba423cad0bf628933c52d951a
LeaBartos <lotalulea@gmail.com>
2025-01-15 15:14:47 +0100

    Program.cs

--- CSHARP/Ucenje/Program.cs
+++ CSHARP/Ucenje/Program.cs
@@ -1,12 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using Ucenje;
-E01UlazIzlaz.Izvedi();
 
+E12Z1.Izvedi();
 
+//E11ForEach.Izvedi();
 
+//E10DoWhilePetlja.Izvedi();
 
+//E09WhilePetlja.Izvedi();
 
+//E08ForPetlja.Izvedi();
+
+//E06Nizovi.Izvedi();
+
+//E05UvjetnoGrananjeSwitch.Izvedi();
+
+//E03Operatori.Izvedi();
+
+//E02Varijable.Izvedi();
+
+//E01Z1.Izvedi();
+
+//E01UlazIzlaz.Izvedi();
 
 
 //Console.WriteLine("Hello, World!");
+
